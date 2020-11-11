ae7b519eae41319bd66127d56fef2710909c04eb
DanielFernandezR <dfernandez@cifpfbmoll.eu>
2020-11-11 17:25:10 +0100

    feat(): Añadir archivo database.php

---
+++ config/database.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$db_name = "php_beginner_crud_level_1";
+$username = "root";
+$password = "1234";
+
+try {
+    $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
+}
+catch(PDOException $exception) {
+    echo "Connection error: " . $exception->getMessage();
+}
